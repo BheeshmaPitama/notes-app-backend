6e0726929cb6133b94850052f97fb4f7c01eaee9
Anubhav Tiwari <mail2anubhavt@gmail.com>
2022-06-11 15:11:48 +0530

    Adding and Updating Notes.

--- src/main/kotlin/tiwari/anubhav/data/routes/NoteRoute.kt
+++ src/main/kotlin/tiwari/anubhav/data/routes/NoteRoute.kt
@@ -1,11 +1,16 @@
 package tiwari.anubhav.data.routes
 
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import tiwari.anubhav.data.collections.Note
 import tiwari.anubhav.data.getNotesForUser
+import tiwari.anubhav.data.saveNote
 
 fun Route.noteRoutes(){
     route("/getNotes"){
@@ -16,6 +21,24 @@ fun Route.noteRoutes(){
                 call.respond(OK,notes)
             }
         }
+    }
 
+    route("/addNote"){
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: ContentTransformationException){
+                    call.respond(BadRequest)
+                    return@post
+                }
+
+                if(saveNote(note)){
+                    call.respond(OK)
+                }else{
+                    call.respond(Conflict)
+                }
+            }
+        }
     }
 }
