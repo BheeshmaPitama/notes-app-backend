88e6e80dfb7d169d62b5673f49569a1926580bbb
Anubhav Tiwari <mail2anubhavt@gmail.com>
2022-06-11 14:33:16 +0530

    Added Authentication Logic.

--- src/main/kotlin/tiwari/anubhav/plugins/Security.kt
+++ src/main/kotlin/tiwari/anubhav/plugins/Security.kt
@@ -1,12 +1,24 @@
 package tiwari.anubhav.plugins
 
-import io.ktor.server.auth.*
-import io.ktor.util.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.auth.*
+import tiwari.anubhav.data.checkPasswordForEmail
 
 fun Application.configureSecurity() {
+    install(Authentication){
+        configureAuth()
+    }
+}
 
-
+private fun AuthenticationConfig.configureAuth(){
+    basic {
+        realm="Note Server"
+        validate {credentials->
+            val email = credentials.name
+            val password = credentials.password
+            if(checkPasswordForEmail(email,password)){
+                UserIdPrincipal(email)
+            }else null
+        }
+    }
 }
